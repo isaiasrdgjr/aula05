74c080d23dfa2999937c1100dcf84e9b21a2d23f
isaiasrdgjr <isaias.rdg@gmail.com>
2025-04-17 20:41:42 -0300

    Exemplo For

---
+++ aula05/exemplo_for.py
@@ -0,0 +1,22 @@
+# for n in range(5):
+#     print('Olá mundo!') 
+
+# for n in range(5):
+#     prin t(n)
+
+# for i in range(5, 20):
+#    print(i)
+
+# for i in range(5, 50, 5):
+#   print(i)
+
+
+# inicio = int(input('Informe o início: '))
+# fim = int(input('Informe o fim: '))
+# for i in range(inicio, fim):
+#   print(i)
+
+
+for n in range(3):
+    nome = input('Informe o nome: ')
+    print(f'O nome da pessoa é: {nome}')
